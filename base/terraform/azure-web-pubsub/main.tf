490117b74289d39b3dc1d301801475457860857c
James Wilson <jwils22@gmail.com>
2022-12-09 12:00:36 -0500

    Added new pubsubs

--- base/terraform/azure-web-pubsub/main.tf
+++ base/terraform/azure-web-pubsub/main.tf
@@ -16,7 +16,41 @@ resource "azurerm_resource_group" "azapi_labs_eu_west" {
   location = "West US"
 }
 
+resource "azurerm_web_pubsub" "example_eu_west" {
+  name                = "tfex-webpubsub_eu_west"
+  location            = azurerm_resource_group.azapi_labs_eu_west.location
+  resource_group_name = azurerm_resource_group.azapi_labs_eu_west.name
+
+  sku      = "Standard_S1"
+  capactiy = 1
+
+  public_network_access_enabled = false
+
+  live_trace {
+    enabled                   = true
+    messaging_logs_enabled    = true
+    connectivity_logs_enabled = false
+  }
+}
+
 resource "azurerm_resource_group" "azapi_labs_us_east" {
   name     = "azapi_labs_us_east"
   location = "East US"
+}
+
+resource "azurerm_web_pubsub" "example_us_east" {
+  name                = "tfex-webpubsub_us_east"
+  location            = azurerm_resource_group.azapi_labs_us_east.location
+  resource_group_name = azurerm_resource_group.azapi_labs_us_east.name
+
+  sku      = "Standard_S1"
+  capactiy = 1
+
+  public_network_access_enabled = false
+
+  live_trace {
+    enabled                   = true
+    messaging_logs_enabled    = true
+    connectivity_logs_enabled = false
+  }
 }
